93627bd464dad22f07dfe90f98d45bcff71a322f
Awills123 <Awills1@babson.edu>
2021-11-07 21:51:32 -0500

    trying to start website

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 """
 Simple "Hello, World" application using Flask
 """
-
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route("/mbta/")
+def hello(name=None):
+    if name:
+        name = name.upper()
+        return render_template("hello.html", name=name)
+    return "Hello"
+
 
 
 if __name__ == '__main__':
