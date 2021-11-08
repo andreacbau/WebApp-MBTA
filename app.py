d8f155709fc5761fed2092293f13463e5298f02f
Awills123 <Awills1@babson.edu>
2021-11-08 10:45:16 -0500

    start on website

--- app.py
+++ app.py
@@ -13,14 +13,26 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/mbta/")
+@app.route("/hello/")
 def hello(name=None):
     if name:
         name = name.upper()
         return render_template("hello.html", name=name)
     return "Hello"
 
-
+@app.route("/mbta/", methods=["GET", "POST"])
+def get_nearest_station():
+    if request.method == "POST":
+        place = float(request.form["a"])
+        nearest = find_stop_near(place)
+        if nearest:
+            return render_template("Nearest_MBTA.html" , 
+            place=place, 
+            nearest=nearest,)
+        else: 
+            return render_template("MBTA_form.html", 
+            error = True)
+    return render_template("MBTA_form.html", error=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
